cbd2a770e7d1477d53fcab01f78453000f28c13a
Hani <haniaboultaif@gmail.com>
2022-12-18 14:40:56 -0300

    Feature Get-Medicos

--- src/main/java/med/voll/api/medico/DadosCadastroMedico.java
+++ src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -1,8 +1,10 @@
 package med.voll.api.medico;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import med.voll.api.endereco.DadosEndereco;
 
 public record DadosCadastroMedico(
@@ -12,8 +14,13 @@ public record DadosCadastroMedico(
         @Email
         String email,
         @NotBlank
-        String crm,
+        String telefone,
         @NotBlank
+        @Pattern(regexp = "\\d{4,6}")
+        String crm,
+        @NotNull
         Especialidade especialidade,
+        @NotNull
+        @Valid
         DadosEndereco endereco ) {
 }
